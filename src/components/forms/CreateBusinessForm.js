52227c97a370e14c468b8e090df326d663006959
tcotts <thomascotter00@gmail.com>
2022-05-08 09:53:27 +0100

    fixed import bugs for Nav in CreateBusinessForm.js

--- src/components/forms/CreateBusinessForm.js
+++ src/components/forms/CreateBusinessForm.js
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
-
+import Nav from 'react-bootstrap/Nav'
 
 const CreateBusinessForm = (props) => {
 
